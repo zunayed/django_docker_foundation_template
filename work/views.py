ebfb950b3f1a8dbf9d25c130ac8b9ddc1f8c5668
Zunayed Ali <zunayed@gmail.com>
2014-02-10 17:27:07 -0500

    added variable for all post to be passed into render function

--- work/views.py
+++ work/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from work.models import Post
 
 
 def work_page(request):
     print request
-    return render(request, 'work.html')
+    return render(request, 'work.html', {
+        'posts': Post.objects.all().order_by('date_created')})
