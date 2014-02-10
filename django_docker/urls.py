a06bdbe64ef508cc29f8c7d51b13cd87a037c063
Zunayed Ali <zunayed@gmail.com>
2014-02-10 17:24:25 -0500

    Modified home to index to match file name

--- django_docker/urls.py
+++ django_docker/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^people/', 'people.views.people_page', name='people'),
     url(r'^work/', 'work.views.work_page', name='work'),
     url(r'^admin/', include(admin.site.urls)),
